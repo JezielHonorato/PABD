afdc6c40c1194244c387cf858cde1da6a27a0029
JezielHonorato <jezielh.ss@gmail.com>
2023-12-03 21:18:09 -0300

    criei o /json pra dividir melhor

--- animatronics/routes/especie.js
+++ animatronics/routes/especie.js
@@ -3,12 +3,22 @@ let router = express.Router();
 let db = require('../utils/db');
 
 router.get('/listar', function(req, res) {
-  let cmd = 'SELECT * FROM tbespecie ORDER BY especie';
+  let cmd = 'SELECT idEspecie, especie FROM tbespecie ORDER BY especie';
   db.query(cmd, [], function(erro, listagem){
     if (erro){
       res.send(erro);
     }
-    res.render('listarEspecie', {resultado: listagem});
+    res.render('listarEspecie',{resultado: listagem});
+  });
+});
+
+router.get('/json', function(req, res) {
+  let cmd = 'SELECT idEspecie, especie FROM tbespecie ORDER BY especie';
+  db.query(cmd, [], function(erro, listagem){
+    if (erro){
+      res.send(erro);
+    }
+    res.json({resultado: listagem});
   });
 });
 
--- animatronics/routes/funcao.js
+++ animatronics/routes/funcao.js
@@ -3,13 +3,24 @@ let router = express.Router();
 let db = require('../utils/db');
 
 router.get('/listar', function(req, res) {
-  let cmd = 'SELECT * FROM tbfuncao ORDER BY funcao';
+  let cmd = 'SELECT idFuncao, funcao FROM tbfuncao ORDER BY funcao';
   db.query(cmd, [], function(erro, listagem){
     if (erro){
       res.send(erro);
     }
     /* Criaremos a view autores-lista.ejs no próximo slide*/
-    res.render('listarFuncao', {resultado: listagem});
+    res.render('listarfuncao',{resultado: listagem});
+  });
+});
+
+router.get('/json', function(req, res) {
+  let cmd = 'SELECT idFuncao, funcao FROM tbfuncao ORDER BY funcao';
+  db.query(cmd, [], function(erro, listagem){
+    if (erro){
+      res.send(erro);
+    }
+    /* Criaremos a view autores-lista.ejs no próximo slide*/
+    res.json({resultado: listagem});
   });
 });
 
